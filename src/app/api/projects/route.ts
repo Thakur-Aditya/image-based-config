e19912c9e47b376035c7047201ea04aa9db7cad8
Thakur-Aditya <87617889+Thakur-Aditya@users.noreply.github.com>
2025-02-05 12:02:36 +0530

    route added - creating new project

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+import { z } from "zod";
 
 export async function GET() {
   try {
@@ -14,6 +15,31 @@ export async function GET() {
     return NextResponse.json(
       { error: "Failed to fetch projects",err },
       { status: 500 }
+    );
+  }
+}
+
+const createProjectSchema = z.object({
+  name: z.string().min(1),
+  description: z.string().optional(),
+});
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { name, description } = createProjectSchema.parse(body);
+    console.log(name, description);
+
+    const project = await prisma.project.create({
+      data: { name, description },
+    });
+
+    return NextResponse.json(project);
+  } catch (error) {
+    console.error("Error creating project:", error);
+    return NextResponse.json(
+      { error: "Invalid project data" },
+      { status: 400 }
     );
   }
 }
